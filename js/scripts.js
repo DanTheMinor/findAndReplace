280ba0dc3ff36d100a9024db0806dc8762ae64e1
Dan <student@epicodus.com>
2015-05-29 10:57:59 -0700

    adjusted first method to take strings rather than arrays of words. Write wordReplace, which takes a string, array of indexes and a replacement word, and replaces all the words at the given locations with that word. spec passes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-function wordFind(array, word) {
+function wordFind(string, word) {
+  var array = string.split(" ")
   var arrayOfIndex = []
   array.forEach(function(element, index) {
     if (element == word) {
@@ -6,4 +7,14 @@ function wordFind(array, word) {
     }
   });
   return arrayOfIndex;
+}
+
+function wordReplace(string, arrayOfIndex, word) {
+  var arrayOfWords = string.split(" ");
+  arrayOfIndex.forEach(function(element, index) {
+    //the element in this case is the index of the word to be replaced in arrOfWords
+    arrayOfWords[element] = word;
+  });
+  string = arrayOfWords.join(" ");
+  return string;
 }
